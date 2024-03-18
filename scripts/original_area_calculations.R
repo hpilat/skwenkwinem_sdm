b64f8c06d35e2d1931bc43efd6d8e7664b4a8af1
hpilat <hpilat@student.ubc.ca>
2024-03-18 16:13:10 -0600

    removed plotting code

--- scripts/original_area_calculations.R
+++ scripts/original_area_calculations.R
@@ -16,14 +16,16 @@ library(tidyterra)
 library(terra)
 library(sf)
 
-# Read in Binary Predictions:
-informed_present_binary <- rast("outputs/skwenkwinem_informed_predict_present_binary.tif")
-bioclim30s_present_binary <- rast("outputs/skwenkwinem_bioclim30s_predict_present_binary.tif")
-bioclim30s_future_binary <- rast("outputs/skwenkwinem_bioclim30s_predict_future_binary.tif")
-# reproject to Albers equal area:
-informed_present_binary <- project(informed_present_binary, "EPSG:3005")
-bioclim30s_present_binary <- project(bioclim30s_present_binary, "EPSG:3005")
-bioclim30s_future_binary <- project(bioclim30s_future_binary, "EPSG:3005")
+# Read in Classified Predictions:
+
+# Present:
+informed_classified <- rast("outputs/informed_classified.tif")
+bioclim_classified <- rast("outputs/bioclim_present_classified.tif")
+model_agreement <- rast("outputs/model_agreement.tif")
+
+# Future:
+bioclim_classified_fut <- rast("outputs/bioclim_classified_fut.tif")
+model_agreement_fut <- rast("outputs/model_agreement_future.tif")
 
 
 
@@ -68,33 +70,12 @@ skeetch_area <- units::set_units(st_area(skeetch_sf_albers), km^2) # 6996 km^2
 
 
 
-# Get area of agreement between Informed and Bioclim30s (present) models:
-
-
-
-# Full study extent:
-# Informed
-# Use binary prediction raster
-plot(informed_present_binary)
-summary(informed_present_binary)
-
-# need to reclassify cells from presence to 1 and pseudoabsence to 0
-# terra::classify requires us to provide a matrix of values from -> values to
-# create a list so 1 (presence) stays as 1 and 2 (pseudoabsence) becomes 0
-matrix_informed_cols <- c(1, 2, 1, 0)
-matrix_informed <- matrix(matrix_informed_cols, ncol = 2)
-matrix_informed
-
-# reclassify values to 1 (presence) and 0 (pseudoabsence)
-informed_classified <- terra::classify(informed_present_binary, matrix_informed)
-plot(informed_classified)
-
-
+# Informed Prediction:
+informed_classified
 
 # calculate area predicted to be suitable by informed model:
 # mask to our study area boundary polygon
 informed_classified_masked <- mask(informed_classified, na_bound_albers)
-
 
 # filter out cells classified as informed (1), removes pseudoabsences (0)
 informed_classified_filt <- informed_classified_masked %>% 
@@ -115,21 +96,8 @@ informed_classified_area <- units::set_units(st_area(informed_classified_sf), km
 # 958 544 km^2 of suitable habitat
 
 
-# Calculate area predicted to be suitable by the Bioclim (present) model:
-# use binary prediction raster:
-plot(bioclim30s_present_binary)
-
-# reclassify cells, starting with creating matrix
-# want values to go from 1 (presence) to 2 (distinguished from informed raster)
-# and 2 (pseudoabsences) to 0 (same as in informed raster)
-matrix_bioclim_cols <- c(1, 2, 2, 0)
-matrix_bioclim <- matrix(matrix_bioclim_cols, ncol=2)
-matrix_bioclim
-
-# classify binary raster according to matrix
-bioclim_classified <- classify(bioclim30s_present_binary, matrix_bioclim)
-plot(bioclim_classified)
-
+# Bioclim Present Prediction:
+bioclim_classified
 
 # calculate area predicted to be suitable by bioclim model:
 # mask to our study extent:
@@ -159,18 +127,8 @@ difference_present_predictions <- bioclim_classified_area - informed_classified_
 # 58 730 km^2 more predicted by bioclim model
 
 
-# add informed_classified and bioclim_classified together to get model_agreement:
-model_agreement <- (informed_classified + bioclim_classified)
-plot(model_agreement)
-# 0 = pseudoabsence
-# 1 = informed prediction of presence
-# 2 = bioclim30s prediction of presence
-# 3 = agreement between both informed and bioclim30s predicted presence
-
-# save to file
-writeRaster(model_agreement, filename = "outputs/model_agreement.tif", overwrite = TRUE)
-
-
+# Model Agreement:
+model_agreement
 # calculate area predicted to be suitable by both informed and bioclim30s (present) models:
 # mask to our study extent:
 model_agreement_masked <- mask(model_agreement, na_bound_albers)
@@ -263,46 +221,6 @@ total_area_bioclim <- ((pseudo_bioclim_area + pseudo_informed_area) - pseudo_dif
 
 
 
-
-# Plotting:
-
-
-
-# need to convert binary_mean from numeric to factor
-summary(model_agreement)
-model_agreement <- as.factor(model_agreement)
-summary(model_agreement)
-plot(model_agreement)
-# 0 = pseudoabsence
-# 1 = informed prediction of presence
-# 2 = bioclim30s prediction of presence
-# 3 = agreement between both informed and bioclim30s predicted presence
-
-agreement_present <- ggplot() +
-  geom_spatraster(data = model_agreement, aes(fill = binary_mean)) +
-  scale_fill_manual(name = NULL, na.translate = FALSE,
-                    labels = c("pseudoabsence", "informed", "bioclim", "both"), 
-                    values = c("grey", "#2A7B8EFF", "#FDE725FF", "#7AD151FF")) +
-  scale_x_continuous(name = "Longitude (°W)", 
-                     # breaks = c(105, 110, 115, 120, 125, 130, 135),
-                     labels = c("135", "130", "125", "120", "115", "110", "105"), 
-                     # limits = c(-103.04, -137.07),
-                     expand = c(0,0)) +
-  # theme(axis.text.x = element_text(angle = 90)) +
-  scale_y_continuous(name = "Latitude (°N)",
-                     #limits = c(50.3, 51.6),
-                     breaks = c(35, 40, 45, 50, 55),
-                     labels = c("35", "40", "45", "50", "55"), 
-                     expand = c(0, 0)) +
-  labs(title = "Area of Agreement", 
-       subtitle = "Informed and Bioclim30s Models (Present)")
-
-agreement_present
-
-ggsave("outputs/agreement_present.png", plot = agreement_present)
-
-
-
 # Skeetchestn Territory Calculations:
 
 
@@ -371,8 +289,8 @@ difference_present_skeetch <- informed_skeetch_area - bioclim_skeetch_area
 
 # calculate area predicted to be suitable by both informed and bioclim30s (present) models:
 
+
 # crop model_agreement to Skeetchestn Territory:
-  # for use in plotting:
 model_agreement_skeetch <- crop(model_agreement, skeetch_extent)
 # mask to Skeetchestn Territory boundary:
 model_agreement_skeetch_masked <- mask(model_agreement_skeetch, skeetch_vectWGS84)
@@ -411,66 +329,13 @@ proportion_bioclim_pres_skeetch <- bioclim_skeetch_area/skeetch_area
 # 38.9 %
 
 
-# Plot area of agreement in Skeetchestn Territory:
-
-
-
-# plot area of agreement not masked to Skeetch with Skeetch polygon overlaid:
-# turn Skeetchestn boundary polygon into lines geometry:
-skeetch_lines <- as.lines(skeetch_vectWGS84)
-
-# convert raster from numeric to factor
-model_agreement_skeetch <- as.factor(model_agreement_skeetch)
-
-model_agreement_skeetch
-# 0 = pseudoabsence
-# 1 = informed prediction of presence
-# 2 = bioclim30s prediction of presence
-# 3 = agreement between both informed and bioclim30s predicted presence
-
-
-skeetch_agreement_present <- ggplot() +
-  geom_spatraster(data = model_agreement_skeetch, aes(fill = binary_mean)) +
-  geom_spatvector(data = skeetch_lines, aes(fill = NULL), colour = "white") +
-  scale_fill_manual(name = NULL, na.translate = FALSE,
-                    labels = c("pseudoabsence", "present", "future", "both"), 
-                    values = c("grey", "#2A7B8EFF", "#FDE725FF", "#7AD151FF")) +
-  scale_x_continuous(name = "Longitude (°W)", 
-                     # breaks = c(120.2, 120.4, 120.6, 120.8, 121.0, 121.2, 121.4),
-                     labels = c("121.4", "121.2", "121.0", "120.8", "120.6", "120.4", "120.2"), 
-                     expand = c(0,0)) +
-  theme(axis.text.x = element_text(angle = 90)) +
-  scale_y_continuous(name = "Latitude (°N)",
-                     breaks = c(50.4, 50.6, 50.8, 51.0, 51.2, 51.4),
-                     labels = c("50.4", "50.6", "50.8", "51.0", "51.2", "51.4"), 
-                     expand = c(0, 0)) +
-  labs(title = "Area of Agreement", 
-       subtitle = "Informed and Bioclim30s Models (Present)")
-
-skeetch_agreement_present
-
-ggsave("outputs/skeetch_agreement_present.png", plot = skeetch_agreement_present)
-
-
-
-# Area of Agreement between Bioclim present and future predictions
-
 
 
 # Bioclim30s Future:
-# use binary prediction raster:
-plot(bioclim30s_future_binary)
 
-# reclassify cells, starting with creating matrix
-# want values to go from 1 (presence) to 2 (distinguished from informed raster)
-# and 2 (pseudoabsences) to 0 (same as in informed raster)
-matrix_bioclim_fut_cols <- c(1, 2, 4, 0)
-matrix_bioclim_fut <- matrix(matrix_bioclim_fut_cols, ncol=2)
-matrix_bioclim_fut
 
-# classify binary raster according to matrix
-bioclim_classified_fut <- classify(bioclim30s_future_binary, matrix_bioclim_fut)
-plot(bioclim_classified_fut)
+
+bioclim_classified_fut
 
 # mask to study extent:
 bioclim_classified_fut_masked <- mask(bioclim_classified_fut, na_bound_vect)
@@ -500,18 +365,13 @@ difference_area_future <- bioclim_classified_fut_area - bioclim_classified_area
 # -111 767 km^2 (less predicted by future model)
 
 
-# add bioclim_classified_fut and bioclim_classified together to get model_agreement:
-model_agreement_fut <- (bioclim_classified + bioclim_classified_fut)
-plot(model_agreement_fut)
-# 0 = pseudoabsence
-# 2 = bioclim30s present prediction of presence
-# 4 = bioclim30s future prediction of presence
-# 6 = agreement between both bioclim30s present and future predicted presence
 
-# save to file
-writeRaster(model_agreement_fut, filename = "outputs/model_agreement_future.tif", overwrite = TRUE)
+# Area of Agreement between Bioclim present and future predictions
+
+
 
 # calculate area of agreement:
+model_agreement_fut
 # mask to study extent:
 model_agreement_fut_masked <- mask(model_agreement_fut, na_bound_vect)
 # Project to Albers equal area projection
@@ -576,43 +436,6 @@ proportion_bioclim_fut <- bioclim_classified_fut_area/na_bound_area
 proportion_pseudoabs_fut <- pseudoabs_fut_area/na_bound_area
 # 0.677
 
-# Plotting:
-
-
-
-# need to convert binary_mean from numeric to factor
-summary(model_agreement_fut)
-model_agreement_fut <- as.factor(model_agreement_fut)
-summary(model_agreement_fut)
-plot(model_agreement_fut)
-# now: 
-# 0 = pseudoabsence
-# 2 = bioclim present prediction
-# 4 = bioclim future prediction
-# 6 = agreement between both bioclim present and future predictions
-
-agreement_future <- ggplot() +
-  geom_spatraster(data = model_agreement_fut, aes(fill = binary_mean)) +
-  scale_fill_manual(name = NULL, na.translate = FALSE,
-                    labels = c("pseudoabsence", "present", "future", "both"), 
-                    values = c("grey", "#2A7B8EFF", "#FDE725FF", "#7AD151FF")) +
-  scale_x_continuous(name = "Longitude (°W)", 
-                     # breaks = c(105, 110, 115, 120, 125, 130, 135),
-                     labels = c("135", "130", "125", "120", "115", "110", "105"), 
-                     # limits = c(-103.04, -137.07),
-                     expand = c(0,0)) +
-  # theme(axis.text.x = element_text(angle = 90)) +
-  scale_y_continuous(name = "Latitude (°N)",
-                     #limits = c(50.3, 51.6),
-                     breaks = c(35, 40, 45, 50, 55),
-                     labels = c("35", "40", "45", "50", "55"), 
-                     expand = c(0, 0)) +
-  labs(title = "Area of Agreement", 
-       subtitle = "Bioclim30s Present and Future Models")
-
-agreement_future
-ggsave("outputs/agreement_future.png", plot = agreement_future)
-
 
 
 # Skeetchestn Territory Calculations:
@@ -624,7 +447,6 @@ bioclim_skeetch_area
 
 # Bioclim Model (future):
 # crop bioclim_classified to Skeetchestn extent:
-# to be used in plotting section
 bioclim_fut_skeetch <- crop(bioclim_classified_fut, skeetch_extent)
 # mask values outside Skeetchestn Territory to NA:
 bioclim_fut_skeetch_masked <- mask(bioclim_fut_skeetch, skeetch_vectWGS84)
@@ -695,95 +517,3 @@ percent_overlap_fut_skeetch <- (model_agreement_fut_skeetch_area / (bioclim_skee
 # proportion of Skeetchestn Territory classified as present by bioclim future model:
 proportion_bioclim_fut_skeetch <- bioclim_fut_skeetch_area/skeetch_area
 # 16.3 %
-
-
-# plot area of agreement not masked to Skeetch with Skeetch polygon overlaid:
-
-plot(model_agreement_fut_skeetch)
-# 0 = pseudoabsence
-# 2 = bioclim present prediction
-# 4 = bioclim future prediction
-# 6 = agreement between both bioclim present and future predictions
-summary(model_agreement_fut_skeetch)
-
-# need to convert binary_mean from numeric to factor
-model_agreement_fut_skeetch <- as.factor(model_agreement_fut_skeetch)
-summary(model_agreement_fut_skeetch)
-
-plot(model_agreement_fut_skeetch)
-# 0 = pseudoabsence
-# 2 = bioclim present prediction
-# 4 = bioclim future prediction
-# 6 = agreement between both bioclim present and future predictions
-
-
-skeetch_agreement_future <- ggplot() +
-  geom_spatraster(data = model_agreement_fut_skeetch, aes(fill = binary_mean)) +
-  geom_spatvector(data = skeetch_lines, aes(fill = NULL), colour = "white") +
-  scale_fill_manual(name = NULL, na.translate = FALSE,
-                    labels = c("pseudoabsence", "present", "future", "both"), 
-                    values = c("grey", "#2A7B8EFF", "#FDE725FF", "#7AD151FF")) +
-  scale_x_continuous(name = "Longitude (°W)", 
-                     # breaks = c(120.2, 120.4, 120.6, 120.8, 121.0, 121.2, 121.4),
-                     labels = c("121.4", "121.2", "121.0", "120.8", "120.6", "120.4", "120.2"), 
-                     # limits = c(120.2, 121.6),
-                     expand = c(0,0)) +
-  theme(axis.text.x = element_text(angle = 90)) +
-  scale_y_continuous(name = "Latitude (°N)",
-                     breaks = c(50.4, 50.6, 50.8, 51.0, 51.2, 51.4),
-                     labels = c("50.4", "50.6", "50.8", "51.0", "51.2", "51.4"), 
-                     expand = c(0, 0)) +
-  labs(title = "Area of Agreement", 
-       subtitle = "Bioclim30s Present and Future Models")
-
-skeetch_agreement_future
-
-ggsave("outputs/skeetch_agreement_future.png", plot = skeetch_agreement_future)
-
-
-
-# Plot the area of agreement plots together:
-
-# Full study extent:
-
-model_agreement
-model_agreement_fut
-
-# create temp rasters so we don't overwrite the originals:
-model_agreement_temp <- model_agreement
-model_agreement_fut_temp <- model_agreement_fut
-
-# change the names of our rasters:
-names(model_agreement_temp) <- "Informed and Bioclim Present"
-names(model_agreement_fut_temp) <- "Bioclim Present and Future"
-
-# create a multilayer raster object:
-agreement_full_extent <- c(model_agreement_temp, model_agreement_fut_temp)
-
-# convert raster from numeric to factor
-agreement_full_extent <- as.factor(agreement_full_extent)
-
-agreement_facet_plot <- ggplot() +
-  geom_spatraster(data = agreement_full_extent) +
-  facet_wrap(~lyr, nrow = 1, ncol = 2) +
-  theme_classic() +
-  theme(axis.line = element_line(colour = "black"),
-        strip.background = element_blank(),
-        panel.border = element_blank()) +
-  scale_fill_manual(name = NULL, na.translate = FALSE,
-                    labels = c("pseudoabsence", "informed", "bioclim present", "informed & bioclim", "bioclim future", "bioclim future & present"), 
-                    values = c("grey", "#2A7B8EFF", "#FDE725FF", "#7AD151FF", "#440154FF", "#414487FF")) +
-  scale_x_continuous(name = "Longitude (°W)", 
-                     # breaks = c(105, 110, 115, 120, 125, 130, 135),
-                     labels = c("135", "130", "125", "120", "115", "110", "105"), 
-                     # limits = c(-103.04, -137.07),
-                     expand = c(0,0)) +
-  # theme(axis.text.x = element_text(angle = 90)) +
-  scale_y_continuous(name = "Latitude (°N)",
-                     #limits = c(50.3, 51.6),
-                     breaks = c(35, 40, 45, 50, 55),
-                     labels = c("35", "40", "45", "50", "55"), 
-                     expand = c(0, 0))
-
-ggsave("outputs/full_extent_cont_plots.png", predictions_continuous_plot, 
-       width = 12, height = 4, units = "in")
